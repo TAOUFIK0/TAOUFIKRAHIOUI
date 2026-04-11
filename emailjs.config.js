3c250fdebbf0431ad6751a96658f1706849b84f0
TAOUFIK0 <taoufik.rahioui@usmba.ac.ma>
2026-04-11 20:50:04 +0100

    Publish EmailJS config for GitHub Pages

---
+++ emailjs.config.js
@@ -0,0 +1,8 @@
+// Fill these with your EmailJS values (public key + service + template)
+// EmailJS dashboard: https://dashboard.emailjs.com/
+
+window.EMAILJS_CONFIG = {
+  publicKey: 'HY3LeHkoXdQatx-uc',
+  serviceId: 'service_dvwfa6h',
+  templateId: 'template_ix1jo3e'
+};
